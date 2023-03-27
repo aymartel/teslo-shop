dd9ac3b51968de93fe813d369cfdef3c577799bd
Andy Martel Karpio <aymartel@gmail.com>
2023-03-27 03:23:16 +0300

    en section

---
+++ context/ui/UiProvider.tsx
@@ -0,0 +1,39 @@
+//fix entries reducer to minuscula
+import React, { FC, useReducer } from 'react';
+import { UiContext, uiReducer } from './';
+
+
+export interface UiState {
+    isMenuOpen: boolean;
+}
+
+
+const UI_INITIAL_STATE: UiState = {
+    isMenuOpen: false,
+}
+
+
+interface Props {
+    children?: React.ReactNode | undefined;
+}
+
+
+export const UiProvider: FC<Props> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
+
+    const toggleSideMenu = () => {
+        dispatch({ type: '[UI] - ToggleMenu' });
+    }
+
+
+    return (<UiContext.Provider value={{
+        ...state,
+
+        // Methods
+        toggleSideMenu,
+    }}>
+        {children}
+    </UiContext.Provider>
+    )
+};
