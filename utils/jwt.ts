e7e3eccad7a9d03dc5cc2c74350115a004bb7ee2
Andy Martel Karpio <aymartel@gmail.com>
2023-03-28 02:32:10 +0300

    end section

---
+++ utils/jwt.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+
+export const signToken = (_id: string, email: string) => {
+    if (!process.env.JWT_SECRET_SEED) {
+        throw new Error("No hay semilla de JWT -REvisar Variable Entorno");
+    }
+    return jwt.sign(
+        { _id, email },
+        process.env.JWT_SECRET_SEED,
+        { expiresIn: '30d' }
+    )
+}
